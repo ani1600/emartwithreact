32430fa45d3a1278d8a38f52e203ae4e3f4b5578
Aniket <aniketsinha161000@gmail.com>
2021-09-06 19:47:47 +0530

    Used gradient to design home background

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="https://www.dmartindia.com/static/media/home-banner.644eeaa9.jpg"
+          alt="here is the banner"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAY7Ddw_8yVpLb8I9iOHtkAMmVGI_KZyU",
+  authDomain: "emartapp-bac09.firebaseapp.com",
+  projectId: "emartapp-bac09",
+  storageBucket: "emartapp-bac09.appspot.com",
+  messagingSenderId: "561594688095",
+  appId: "1:561594688095:web:c679e4f03c0c6e838d31ea",
+  measurementId: "G-XDJMWE1MLJ",
+};
+ 
